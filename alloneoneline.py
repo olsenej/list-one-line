b56215d39f22c2c29f5e626a257d55b3be2a2ca9
Edward Olsen <olsenedw@msu.edu>
2013-01-10 15:58:22 -0500

    Update alloneoneline.py
    Made it less rough. Now it will output to the same dir that your source file is in.

--- alloneoneline.py
+++ alloneoneline.py
@@ -2,15 +2,18 @@
 #Edward Olsen
 #12/20/12
 
-file=raw_input('File: ')
+import os,sys
+print 'CWD: ' + os.getcwd()
+file=raw_input('File to work on: ')
 if file == '':
-  file='dbs.txt'
+	file='C:\Users\edward\Documents\dbs.txt' #Hardcoded for my leisure
+
 input=open(file, 'r')
-output=open('C:\Users\edward\Documents\output.txt', 'w')
+output=open(file[0:file.rfind('\\')+1] + 'output.txt', 'w')
 
 for line in input:
 	output.write(line.rstrip('\n'))
 	output.write(" ")
-
+print 'Output written to ' + file[0:file.rfind('\\')+1] + 'output.txt'
 input.close()
 output.close()
